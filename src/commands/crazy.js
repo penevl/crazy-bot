24c73fd0a5e5f4bc1d5263332bffe81f53754842
penevl <penevl@protonmail.com>
2023-08-23 17:01:56 +0300

    Forgot to add the actual logger file to git

---
+++ src/commands/crazy.js
@@ -0,0 +1,69 @@
+const { logger } = require("./logger");
+
+function main(client) {
+    client.on("messageCreate", (msg) => {
+        if (!msg.author.bot) {
+            if (
+                msg.content.toLowerCase() != "crazy?" &&
+                msg.content.toLowerCase().includes("crazy")
+            ) {
+                const reply = "Crazy? I was crazy once.";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+            if (msg.content.toLowerCase() == "crazy?") {
+                const reply = "I was crazy once.";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+            if (
+                msg.content.toLowerCase() == "i was crazy once" ||
+                msg.content.toLowerCase() == "i was crazy once." ||
+                msg.content.toLowerCase() == "i was crazy once!"
+            ) {
+                const reply = "They locked me in a room";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+            if (
+                msg.content.toLowerCase() == "they locked me in a room" ||
+                msg.content.toLowerCase() == "they locked me in a room." ||
+                msg.content.toLowerCase() == "they locked me in a room!"
+            ) {
+                const reply = "A rubber room";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+            if (
+                msg.content.toLowerCase() == "a rubber room" ||
+                msg.content.toLowerCase() == "a rubber room." ||
+                msg.content.toLowerCase() == "a rubber room!"
+            ) {
+                const reply = "A rubber room with rats";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+            if (
+                msg.content.toLowerCase() == "a rubber room with rats" ||
+                msg.content.toLowerCase() == "a rubber room with rats." ||
+                msg.content.toLowerCase() == "a rubber room with rats!"
+            ) {
+                const reply = "The rats make me crazy";
+                logger.info(
+                    `Replying to ${msg.author.username}'s ${msg} with ${reply}`
+                );
+                msg.channel.send(reply);
+            }
+        }
+    });
+}
---
+++ src/logger.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const winston = require("winston");
+const LOG_LEVEL = process.env.LOG_LEVEL;
+
+if (LOG_LEVEL == null || LOG_LEVEL == "") {
+    console.error("LOG_LEVEL not deffined");
+    process.exit(-1);
+}
+
+/**
+ * @returns {winston.Logger}
+ */
+const logger = winston.createLogger({
+    level: LOG_LEVEL,
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
+    transports: [
+        new winston.transports.File({
+            filename: `${new Date().toISOString()}.log`,
+            dirname: "logs",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json()
+            ),
+        }),
+        new winston.transports.Console({
+            format: winston.format.cli(),
+        }),
+    ],
+});
+
+module.exports = { logger };
