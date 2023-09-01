cda80a48c670f403c79e523716c242b1ea190d21
penevl <penevl@protonmail.com>
2023-09-02 00:51:59 +0300

    Fully moved promote command to event

--- src/commands/promote.js
+++ src/commands/promote.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { logger } = require("../utils/logger");
 const { isAdmin, getUserRoles } = require("../utils/discord/user");
 const { ChatInputCommandInteraction } = require("discord.js");
+const { PromoteUser } = require("../utils/discord/PromoteUser");
 
 function main(client) {
     logger.info("Registered promote command(s)");
@@ -9,139 +10,70 @@ function main(client) {
         if (!interaction.isChatInputCommand()) return;
 
         if (interaction.commandName === "promote") {
+            await interaction.deferReply({ ephemeral: true });
+
+            const mentorRole = process.env.MENTOR_ROLE;
+            const newMemberId = interaction.options.get("user").value;
+            const newMember = await interaction.guild.members.fetch(
+                newMemberId
+            );
+            const mentor = await interaction.guild.members.fetch(
+                interaction.user
+            );
+
+            const promoteUser = new PromoteUser();
+
+            promoteUser.on("promotion", (displayName) => {
+                interaction.editReply({
+                    content: `Successfuly promoted ${displayName}`,
+                });
+            });
+
+            promoteUser.on("reverse", (displayName) => {
+                interaction.editReply({
+                    content: `Successfuly reverse promoted ${displayName}`,
+                });
+            });
+
+            promoteUser.on("error", (err) => {
+                interaction.editReply({
+                    content: err.message,
+                });
+            });
+
             var reversed = false;
             try {
                 reversed = interaction.options.get("reverse").value;
             } catch (error) {}
-            await interaction.deferReply({ ephemeral: true });
             if (reversed) {
-                reversePromote(interaction);
+                if (isAdmin(mentor)) {
+                    promoteUser.reverse(newMember);
+                } else {
+                    logger.warn(
+                        `${mentor.displayName} tried to use the reverse promote command with insufficient privileges`
+                    );
+                    interaction.editReply({
+                        content: `Insufficient privileges to use reverse promote command`,
+                    });
+                }
             } else {
-                promote(interaction);
+                if (
+                    getUserRoles(mentor).includes(mentorRole) ||
+                    isAdmin(mentor)
+                ) {
+                    promoteUser.promote(newMember);
+                } else {
+                    logger.warn(
+                        `${mentor.displayName} tried to use the promote command with insufficient privileges`
+                    );
+                    interaction.editReply({
+                        content: `Insufficient privileges to use promote command`,
+                    });
+                    return;
+                }
             }
         }
     });
 }
 
-/**
- *
- * @param {ChatInputCommandInteraction} interaction
- */
-async function promote(interaction) {
-    const newcomerRole = process.env.NEWCOMER_ROLE;
-    const promotionRole = process.env.PROMOTION_ROLE;
-    const imposterRole = process.env.IMPOSTER_ROLE;
-    const mentorRole = process.env.MENTOR_ROLE;
-    const newMemberId = interaction.options.get("user").value;
-    const newMember = await interaction.guild.members.fetch(newMemberId);
-    const mentor = await interaction.guild.members.fetch(interaction.user);
-
-    if (getUserRoles(mentor).includes(mentorRole) || isAdmin(mentor)) {
-        logger.warn(
-            `${mentor.displayName} tried to use the promote command with insufficient privileges`
-        );
-        interaction.editReply({
-            content: `Insufficient privileges to use promote command`,
-        });
-        return;
-    }
-    newMember.roles
-        .remove(
-            [newcomerRole, imposterRole],
-            `Member was promoted by ${interaction.client.user.username}`
-        )
-        .then(() => {
-            newMember.roles
-                .add(
-                    promotionRole,
-                    `Member was promoted by ${interaction.client.user.username}`
-                )
-                .then(() => {
-                    logger.info(
-                        `${mentor.displayName} has promoted ${newMember.displayName}`
-                    );
-                    interaction.editReply({
-                        content: `Successfuly promoted ${newMember.displayName}`,
-                    });
-                })
-                .catch((err) => {
-                    logger.error(
-                        `${mentor.displayName} has tried to promote ${newMember.displayName} but the bot failed to add the promotion role`
-                    );
-                    logger.error(err);
-                    interaction.editReply({
-                        content: `Tried to promote ${newMember.displayName} but the bot failed to add the promotion role`,
-                    });
-                });
-        })
-        .catch((err) => {
-            logger.error(
-                `${mentor.displayName} has tried to promote ${newMember.displayName} but the newcomer role(s) failed to be removed`
-            );
-            logger.error(err);
-            interaction.editReply({
-                content: `Tried to promote ${newMember.displayName} but the newcomer role(s) failed to be removed`,
-            });
-        });
-}
-
-/**
- *
- * @param {ChatInputCommandInteraction} interaction
- */
-async function reversePromote(interaction) {
-    const newcomerRole = process.env.NEWCOMER_ROLE;
-    const promotionRole = process.env.PROMOTION_ROLE;
-    const newMemberId = interaction.options.get("user").value;
-    const newMember = await interaction.guild.members.fetch(newMemberId);
-    const mentor = await interaction.guild.members.fetch(interaction.user);
-
-    if (isAdmin(mentor)) {
-        newMember.roles
-            .remove(
-                [promotionRole],
-                `Member was promoted by ${interaction.client.user.username}`
-            )
-            .then(() => {
-                newMember.roles
-                    .add(
-                        newcomerRole,
-                        `Member was reverse promoted by ${interaction.client.user.username}`
-                    )
-                    .then(() => {
-                        logger.info(
-                            `${mentor.displayName} has reverse promoted ${newMember.displayName}`
-                        );
-                        interaction.editReply({
-                            content: `Successfuly reverse promoted ${newMember.displayName}`,
-                        });
-                    })
-                    .catch((err) => {
-                        logger.error(
-                            `${mentor.displayName} has tried to reverse promote ${newMember.displayName} but the bot failed to add the promotion role`
-                        );
-                        logger.error(err);
-                        interaction.editReply({
-                            content: `Tried to reverse promote ${newMember.displayName} but the bot failed`,
-                        });
-                    });
-            })
-            .catch((err) => {
-                logger.error(
-                    `${mentor.displayName} has tried to reverse promote ${newMember.displayName} but failed`
-                );
-                logger.error(err);
-                interaction.editReply({
-                    content: `Tried to reverse promote ${newMember.displayName} but failed`,
-                });
-            });
-    } else {
-        logger.warn(
-            `${mentor.displayName} tried to use the reverse promote command with insufficient privileges`
-        );
-        interaction.editReply({
-            content: `Insufficient privileges to use reverse promote command`,
-        });
-    }
-}
 module.exports = { main };
---
+++ src/utils/discord/PromoteUser.js
@@ -0,0 +1,95 @@
+const { GuildMember } = require("discord.js");
+const { logger } = require("../logger");
+const EventEmitter = require("node:events");
+
+class PromoteUser extends EventEmitter {
+    /**
+     *
+     * @param {GuildMember} user
+     */
+    promote(user) {
+        const newcomerRole = process.env.NEWCOMER_ROLE;
+        const promotionRole = process.env.PROMOTION_ROLE;
+        const imposterRole = process.env.IMPOSTER_ROLE;
+
+        user.roles
+            .remove([newcomerRole, imposterRole])
+            .then(() => {
+                user.roles
+                    .add(promotionRole)
+                    .then(() => {
+                        logger.info(`Promoted ${user.displayName}`);
+                        this.emit("promotion", user.displayName);
+                    })
+                    .catch((err) => {
+                        logger.error(
+                            `Tried to promote ${user.displayName} but the bot failed to add the promotion role`
+                        );
+                        logger.error(err);
+                        this.emit(
+                            "error",
+                            new Error(
+                                `Tried to promote ${user.displayName} but the bot failed to add the promotion role`
+                            )
+                        );
+                    });
+            })
+            .catch((err) => {
+                logger.error(
+                    `Tried to promote ${user.displayName} but the newcomer role(s) failed to be removed`
+                );
+                logger.error(err);
+                this.emit(
+                    "error",
+                    new Error(
+                        `Tried to promote ${user.displayName} but the newcomer role(s) failed to be removed`
+                    )
+                );
+            });
+    }
+
+    /**
+     *
+     * @param {GuildMember} user
+     */
+    reverse(user) {
+        const newcomerRole = process.env.NEWCOMER_ROLE;
+        const promotionRole = process.env.PROMOTION_ROLE;
+        user.roles
+            .remove([promotionRole], `${user.displayName} was reverse promoted`)
+            .then(() => {
+                user.roles
+                    .add(newcomerRole)
+                    .then(() => {
+                        logger.info(`Reverse promoted ${user.displayName}`);
+                        this.emit("reverse", user.displayName);
+                    })
+                    .catch((err) => {
+                        logger.error(
+                            `Tried to reverse promote ${user.displayName} but the bot failed to add the promotion role`
+                        );
+                        logger.error(err);
+                        this.emit(
+                            "error",
+                            new Error(
+                                `Tried to reverse promote ${user.displayName} but the bot failed`
+                            )
+                        );
+                    });
+            })
+            .catch((err) => {
+                logger.error(
+                    `${mentor.displayName} has tried to reverse promote ${user.displayName} but failed`
+                );
+                logger.error(err);
+                this.emit(
+                    "error",
+                    new Error(
+                        `Tried to reverse promote ${user.displayName} but failed`
+                    )
+                );
+            });
+    }
+}
+
+module.exports = { PromoteUser };
