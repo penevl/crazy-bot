98a0450bf03f7dfea935953a5ff8ce2421a171ee
penevl <penevl@protonmail.com>
2023-09-01 19:47:13 +0300

    Added function to alter config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const { logger } = require("./logger");
-const { readFileSync } = require("fs");
+const { readFileSync, writeFile } = require("fs");
 
 function getConfig(config) {
     const configFile = readFileSync(
@@ -22,13 +22,23 @@ function loadInitial() {
     const json = JSON.parse(configFile);
     const obj = Object(json);
     for (const [key, value] of Object.entries(obj)) {
-        // logger.debug(`Setting ${key} to ${value}`)
-        process.env[key] = value
+        logger.debug(`Setting ${key} to ${value}`);
+        process.env[key] = value;
     }
 }
 
 function setConfig(config, value) {
     process.env[config] = value;
+    const configFile = readFileSync(
+        "/home/elduko/projects/crazy-bot/config.json",
+        {
+            encoding: "utf-8",
+        }
+    );
+    var json = JSON.parse(configFile);
+    json[config] = value;
+    const newJson = JSON.stringify(json);
+    writeFile("/home/elduko/projects/crazy-bot/config.json", newJson, () => {});
 }
 
 module.exports = { getConfig, setConfig, loadInitial };
